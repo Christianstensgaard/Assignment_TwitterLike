625f5d590cbeb3d8925ca4cff315a18efe30962f
CHJOR <christian.lsj@outlook.dk>
2024-10-19 13:56:12 +0200

    Addded Request model

--- lib/Service/AService.cs
+++ lib/Service/AService.cs
@@ -1,19 +1,32 @@
 ﻿namespace Service;
-
+using Service.Models;
 public abstract class AService
 {
   /// <summary>
   /// When the Service is called this will be called from X
   /// </summary>
   protected abstract void OnRequest();
-
   /// <summary>
   /// Running once on Start
   /// </summary>
   protected abstract void OnStart(ServiceConfig ServiceSettings);
-
   /// <summary>
   /// Running once on close
   /// </summary>
   protected abstract void OnClose();
+
+
+
+  protected void WriteRequest(){
+    // writer request to 
+  }
+
+
+  protected void CreateRequest(){
+    //- Create a request to be sendt over the message bus
+  }
+
+  //- Storing the Service information.
+  RequestModel? requestModel;
+
 }
