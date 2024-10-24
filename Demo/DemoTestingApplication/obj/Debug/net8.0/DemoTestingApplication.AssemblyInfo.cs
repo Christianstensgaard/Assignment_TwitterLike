53370d5bdeaf2d07ef86fbe9f64ac82483b93510
Christian <christian.lsj@outlook.dk>
2024-10-24 23:03:32 +0200

    Created Services

--- Demo/DemoTestingApplication/obj/Debug/net8.0/DemoTestingApplication.AssemblyInfo.cs
+++ Demo/DemoTestingApplication/obj/Debug/net8.0/DemoTestingApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoTestingApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37633ea5f48d93fdd22086de1505c0d920256007")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84c668b34fd3e60f5fa5cf7a205be92eec0860f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoTestingApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoTestingApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ services/PostService/Program.cs
@@ -0,0 +1,63 @@
+﻿using Service;
+
+ToolBox.RunTime.ServiceClientName = "PostService";
+ToolBox.AddService(new CreatePost());
+ToolBox.AddService(new UpdatePost());
+ToolBox.AddService(new DeletePost());
+ToolBox.RunTime.Start("127.0.0.1", 20200);
+
+
+
+public class ValidatePost : ServiceFunction
+{
+    public override void OnInit(FunctionConfig config)
+    {
+      config.FunctionName = "ValidatePost";
+    }
+
+    public override void OnRequest()
+    {
+      
+    }
+}
+
+
+
+public class CreatePost : ServiceFunction
+{
+    public override void OnInit(FunctionConfig config)
+    {
+      config.FunctionName = "CreatePost";
+    }
+
+    public override void OnRequest()
+    {
+      System.Console.WriteLine("Creating Post!");
+    }
+}
+
+public class UpdatePost : ServiceFunction
+{
+    public override void OnInit(FunctionConfig config)
+    {
+      config.FunctionName = "UpdatePost";
+    }
+
+    public override void OnRequest()
+    {
+      System.Console.WriteLine("Updating post!");
+    }
+}
+
+public class DeletePost : ServiceFunction
+{
+    public override void OnInit(FunctionConfig config)
+    {
+        config.FunctionName = "DeletePost";
+    }
+
+    public override void OnRequest()
+    {
+      System.Console.WriteLine("Deleting post");
+    }
+}
