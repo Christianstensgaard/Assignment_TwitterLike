c07d48e0f7bad79e760cbb73fecd0dd7fd0a1dd1
CHJOR <christian.lsj@outlook.dk>
2024-11-06 18:12:30 +0100

    Project reset

--- PostService/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using PostService.Controllers;
-using Service;
-
-ToolBox.RunTime.ServiceClientName = "PostService";
-ToolBox.AddService(new ValidatePost());
-ToolBox.AddService(new CreatePost());
-ToolBox.AddService(new UpdatePost());
-ToolBox.AddService(new DeletePost());
-ToolBox.RunTime.Start("message_server", 20200);
